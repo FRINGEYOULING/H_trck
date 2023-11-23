f96c7baa39d731a1c0ffe74e9821577d8673a5b8
刘政烽 <3280736583@qq.com>
2023-11-23 14:13:12 +0800

    添加注释

--- include/HAL.h
+++ include/HAL.h
@@ -1,22 +1,22 @@
-#ifndef __HAL_H
-#define __HAL_H
+#ifndef __HAL_H  // 防止头文件重复包含
+#define __HAL_H  // 定义宏
 
-#include <Arduino.h>
-#include "HAL.h"
-#ifdef __cplusplus
-extern "C" {
+#include <Arduino.h>  // 包含Arduino库
+#include "HAL.h"  // 包含自定义的HAL库
+#ifdef __cplusplus  // 如果是C++编译器
+extern "C" {  // 以C语言方式编译
 #endif
-#include "lvgl.h"
+#include "lvgl.h"  // 包含lvgl库
 
-namespace HAL
+namespace HAL  // 定义HAL命名空间
 {
-    void HAL_Init(void);
-    void GPS_Update(void);
-    void Time_init(void);
-    void Encoder_Init(void);
-    int32_t Encoder_GetDiff(void);
+    void HAL_Init(void);  // HAL初始化函数
+    void GPS_Update(void);  // GPS更新函数
+    void Time_init(void);  // 时间初始化函数
+    void Encoder_Init(void);  // 编码器初始化函数
+    int32_t Encoder_GetDiff(void);  // 获取编码器差值函数
 }
-#ifdef __cplusplus
-} /*extern "C"*/
+#ifdef __cplusplus  // 如果是C++编译器
+} /*extern "C"*/  // 结束以C语言方式编译
 #endif
-#endif 
+#endif  // 结束防止头文件重复包含
--- lib/LV_UI/Livemap.h
+++ lib/LV_UI/Livemap.h
@@ -5,23 +5,43 @@
 #ifndef LVGL_LIVEMAP_H
 #define LVGL_LIVEMAP_H
 
+#pragma once
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #include "lvgl.h"
 
-
-
+// 创建实时地图
+// root: 父对象
+// tileNum: 瓦片数量
 void Live_Map_Create(lv_obj_t* root, uint32_t tileNum);
+
+// 创建样式
 void Style_Create();
+
+// 创建地图
+// par: 父对象
+// tileNum: 瓦片数量
 void Map_Create(lv_obj_t* par, uint32_t tileNum);
+
+// 创建缩放控制
+// par: 父对象
 void ZoomCtrl_Create(lv_obj_t* par);
+
+// 创建运动信息
+// par: 父对象
 void SportInfo_Create(lv_obj_t* par);
+
+// 卸载实时地图
 void livmap_uninstall(void);
+
+// 安装实时地图
 void livmap_install(void);
+
 #ifdef __cplusplus
-} /*extern "C"*/
+}
 #endif
 
 #endif //LVGL_LIVEMAP_H
